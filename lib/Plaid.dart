121aa6a3659c30f99ddd7ef34775cc85b25246f8
RicardoShepherd <106644200+RicardoShepherd@users.noreply.github.com>
2025-03-27 16:50:00 -0500

    new file plaid

---
+++ lib/Plaid.dart
@@ -0,0 +1,18 @@
+import 'package:plaid/plaid.dart';
+import 'dart:io';
+
+class PlaidClient {
+  static final PlaidApi _plaidApi = PlaidApi(
+    Configuration(
+      basePath: PlaidEnvironments.sandbox,
+      baseOptions: BaseOptions(
+        headers: {
+          'PLAID-CLIENT-ID': Platform.environment['PLAID_CLIENT_ID']!,
+          'PLAID-SECRET': Platform.environment['PLAID_SECRET']!,
+        },
+      ),
+    ),
+  );
+
+  static PlaidApi get client => _plaidApi;
+}
